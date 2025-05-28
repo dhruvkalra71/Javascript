79a3f5345bac4b8bf77c32893d893a7e49b7f59f
dhruvkalra71 <166701601+dhruvkalra71@users.noreply.github.com>
2025-05-28 19:55:18 +0000

    Learnt variables in js

---
+++ 01Basic/variables.js
@@ -0,0 +1,11 @@
+const id = 53
+let PersonName = "Dhruv"
+var password = "helloWorld" //Don't use
+city = "Noida"
+let state;
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([id, PersonName, password, city, state])
