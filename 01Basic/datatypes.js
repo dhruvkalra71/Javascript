5f268a956de39f7210ac3383eb73dd70c1cdf438
dhruvkalra71 <166701601+dhruvkalra71@users.noreply.github.com>
2025-05-28 20:19:47 +0000

    Different datatypes and their uses

---
+++ 01Basic/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // Doesn't work since we are using nodeJS and not browser
+
+console.log(3+3);
+
+let name = "dhruv"
+let age = 20
+let adult = true
+
+/* 
+number - 2^53
+bigInt - for large representation of number
+string - ""
+boolean - true/false
+null - standaline value
+undefined
+symbol - unique
+
+object - advance level
+*/
+
+console.log(typeof null)
+console.log(typeof undefined);
+console.log(typeof adult);
+
