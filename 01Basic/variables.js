d043cabb14c5f990e234b357cad2d37f7b2b2422
dhruvkalra71 <166701601+dhruvkalra71@users.noreply.github.com>
2025-05-28 20:04:52 +0000

    Variables types

--- 01Basic/variables.js
+++ 01Basic/variables.js
@@ -1,8 +1,8 @@
-const id = 53
-let PersonName = "Dhruv"
+const id = 53 // Stays constant - Doesn't change
+let PersonName = "Dhruv" // Preferrable
 var password = "helloWorld" //Don't use
 city = "Noida"
-let state;
+let state; //Undefined
 
 /*
 Prefer not to use var because of issue in block scope and functional scope
